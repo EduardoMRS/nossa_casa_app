f2ec4dd596d003f315a3484b29bf8594a8cb531d
Eduardo Marcos R da Silva <119127074+EduardoMRS@users.noreply.github.com>
2026-09-01 22:01:35 -0400

    Isolate global Bible version test

--- tests/Feature/GlobalBibleAccessTest.php
+++ tests/Feature/GlobalBibleAccessTest.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Inertia\Testing\AssertableInertia as Assert;
 
 test('a user without church can open every configured Bible version', function () {
+    Cache::forget('bible-api.versions.v2');
     Http::fake([
         '*/bibles.json' => Http::response([]),
     ]);
